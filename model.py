c2593eeb7c862212c467ecd6cb97cdbb31bedc22
LiangJunyi-010 <liangjunyi010@gmail.com>
2023-10-01 11:46:28 +0800

    [feat]EarlyStopping

--- model.py
+++ model.py
@@ -23,6 +23,12 @@ preprocessor = ColumnTransformer(
         ('cat', OneHotEncoder(drop='first'), ['Ship Routine', 'Ship type'])
     ])
 
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss',  # 监控验证集上的损失
+    patience=10,  # 如果连续 10 个 epoch 验证损失没有改善，就停止训练
+    restore_best_weights=True  # 恢复最佳权重
+)
+
 # Drop the datetime columns after creating the 'Date Difference' feature
 X = df.drop(columns=['Start Date', 'Contract Arrival Time', 'Delay'])
 X = preprocessor.fit_transform(X)
@@ -35,14 +41,17 @@ y = tf.keras.utils.to_categorical(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = models.Sequential([
-    layers.Dense(32, activation='relu', input_shape=(X_train.shape[1],)),
+    layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(32, activation='relu'),
     layers.Dense(5, activation='softmax')
 ])
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Train the Model
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.1)
+model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.1,callbacks=[early_stopping])
 
 # Example new_data, you will replace it with actual new data
 new_data = pd.DataFrame({
